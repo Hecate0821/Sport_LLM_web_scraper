9ae520ec02e88fd4613ff256c3de1550eed92e30
Hecate <114854555+Hecate0821@users.noreply.github.com>
2023-07-27 23:05:47 +0800

    Update progressChecker.py

--- progressChecker.py
+++ progressChecker.py
@@ -1,10 +1,3 @@
-import time
-
-
-# url = 'https://theathletic.com/'
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.35'}
-
 const_local_path = '/Users/hecate/Downloads/BR/'
 
 start_page = int(140000)
@@ -28,6 +21,5 @@ def check_progress():
         print('Thread ' + str(i) + ': ' + str(now) + ' / ' + str(start_page + i * workload) + ' Progress: ' + str(
             percentage) + '%')
 
-    time.sleep(5)
 
 check_progress()
