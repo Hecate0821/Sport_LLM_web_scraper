ca6b009c3015e5574a89f627ccfcdefb6132f947
Hecate <114854555+Hecate0821@users.noreply.github.com>
2023-07-27 23:33:47 +0800

    delete checkprogress

--- asapSport_scraper.py
+++ asapSport_scraper.py
@@ -19,25 +19,6 @@ end_page = int(10100000)
 local_path = const_local_path + str(start_page) + '_to_' + str(end_page) + '/'
 
 
-def check_progress():
-    workload = int((end_page - start_page) / 100)
-
-    for i in range(1, 101):
-        filename = local_path + 'log_' + str(start_page + (i - 1) * workload) + '_' + str(
-            start_page + i * workload) + '.txt'
-
-        f = open(filename, 'r')
-
-        now = int(f.readline().rstrip())
-
-        percentage = (int(now) - start_page - (i - 1) * workload) / workload * 100
-
-        print('Thread ' + str(i) + ': ' + str(now) + ' / ' + str(start_page + i * workload) + ' Progress: ' + str(
-            percentage) + '%')
-
-    time.sleep(5)
-
-
 def save_log(start, end, now):
     log_path = local_path + 'log/'
     
@@ -420,9 +401,6 @@ def save_as_txt(file_name, file_content):
     else:
         pass
 
-
-# for checking the progress on each thread:
-# check_progress()
 
 # start
 if not os.path.exists(local_path):
