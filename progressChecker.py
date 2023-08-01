37416da709d63c2f283667bb8c8cd5e7dd07a0fe
Hecate <114854555+Hecate0821@users.noreply.github.com>
2023-08-01 18:33:30 +0800

    Update progressChecker.py

--- progressChecker.py
+++ progressChecker.py
@@ -5,7 +5,7 @@ end_page = int(38200500)
 
 local_path = const_local_path + str(start_page) + '_to_' + str(end_page) + '/'
 
-thread_num = int(300)
+thread_num = int(500)
 
 def check_progress():
     workload = int((end_page - start_page) / thread_num)
