85a8b388b337513009b82921268bb171d41a47f0
Hecate <114854555+Hecate0821@users.noreply.github.com>
2023-11-11 14:54:49 +0800

    Update tA.py

--- tA.py
+++ tA.py
@@ -132,7 +132,7 @@ def get_content(page_num):
             return article
 
         else:
-            article.set_content('response code is :\n' + str(response_code) + '\n + content is :\n' + story)
+            article.set_content(story)
             article.set_type('success')
             return article
 
